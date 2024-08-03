ab70bb4ccc011138e6aefeb0558920324eb121e6
zeraibiyoucef <zeraibiyoucef1@gmail.com>
2024-08-03 21:00:30 +0100

    fixing the problemsof the seconde fixing of the optimasation

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,18 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-import withPlugins from 'next-compose-plugins';
-import optimizedImages from 'next-optimized-images';
-
+ 
 const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-            'images.unsplash.com',
-            'images.pexels.com',
-            'uploadthing.com',
-            'utfs.io',
-            'img.clerk.com',
-            'subdomain',
-            'files.stripe.com',
-        ], // Add the required hostnames here
-    },
+    images:{
+        domains: ['images.unsplash.com', 'images.pexels.com', 'uploadthing.com',
+        'utfs.io',
+        'img.clerk.com',
+        'subdomain',
+        'files.stripe.com',] ,// Add the required hostnamesz here
+        formats: ['image/avif', 'image/webp'], // Optional: specify formats
+    }
+    
 };
 
-export default withPlugins([
-    [optimizedImages, {
-      /* config for next-optimized-images */
-        handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
-        optimizeImages: true,
-        optimizeImagesInDev: true,
-    }]
-], withNextIntl(nextConfig));
+
+export default withNextIntl(nextConfig);
